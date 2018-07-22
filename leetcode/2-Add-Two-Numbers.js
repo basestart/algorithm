40c4a2ae401cf9437ac15ca51158f930f026df3d
fridego <931035186@qq.com>
2018-07-22 12:30:27 +0800

    feat: add add-two-numbers

---
+++ leetcode/2-Add-Two-Numbers.js
@@ -0,0 +1,40 @@
+/**
+ * 
+    给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+
+    你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+    示例：
+
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+module.exports = function addTwoNumbers(l1, l2, carry) {
+    let tmp = carry || 0;
+    let [v1, v2] = [l1.val, l2.val];
+    let resNode = new ListNode((v1 + v2 + tmp) % 10);
+    let nextcarry = v1 + v2 + tmp - 9 > 0 ? 1 : 0;
+    if(l1.next || l2.next || nextcarry) {
+        l1 = l1.next || new ListNode(0);
+        l2 = l2.next || new ListNode(0);
+        resNode.next = addTwoNumbers(l1, l2, nextcarry);
+    };
+    return resNode;
+};
--- test/2-Add-Two-Numbers.test.js
+++ test/2-Add-Two-Numbers.test.js
@@ -1,11 +1,6 @@
 const addTwoNumbers = require('../leetcode/2-Add-Two-Numbers');
 
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-
-test('addTwoNumbers((2 -> 4 -> 3) + (5 -> 6 -> 4)) expect 7 -> 0 -> 8', () => {
+test ('addTwoNumbers((2 -> 4 -> 3) + (5 -> 6 -> 4)) expect 7 -> 0 -> 8', () => {
     expect(addTwoNumbers({
         val: 2, next: {
             val: 4,
@@ -33,4 +28,29 @@ test('addTwoNumbers((2 -> 4 -> 3) + (5 -> 6 -> 4)) expect 7 -> 0 -> 8', () => {
             }
         }
     });
+})
+
+test('addTwoNumbers(0->1, 0->9) expect 0->0->1', () => {
+    expect(addTwoNumbers({
+        val: 0,
+        next: {
+            val: 1,
+            next: null
+        }
+    }, {
+        val: 0,
+        next: {
+            val: 9,
+            next: null
+        }
+    })).toEqual({
+        val: 0,
+        next: {
+            val: 0,
+            next: {
+                val: 1,
+                next: null
+            }
+        }
+    })
 })
