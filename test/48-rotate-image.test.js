7ef52c628e46b901c11ebd70102ef8aca5c119a4
fridego <931035186@qq.com>
2018-07-23 20:24:24 +0800

    feature: add rotate image

---
+++ test/48-rotate-image.test.js
@@ -0,0 +1,29 @@
+const rotate = require('../leetcode/48-rotate-image');
+
+test(`rotate[] to be []`, () => {
+    expect(rotate([])).toEqual([]);
+})
+
+test('rotate [[1, 2], [3, 4]] to be [[3, 1], [4, 2]]', () => {
+    expect(rotate([[1, 2], [3, 4]])).toEqual([[3, 1], [4, 2]]);
+})
+
+test(`rotate [
+    [1, 2, 3],
+    [4, 5, 6], 
+    [7, 8, 9]
+] tobe [
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+]`, () => {
+    expect(rotate([
+        [1, 2, 3],
+        [4, 5, 6], 
+        [7, 8, 9]
+    ])).toEqual([
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3]
+    ])
+})
