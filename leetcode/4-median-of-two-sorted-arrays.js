e2cd91952c95be8d0e783ecaaa3f5f8946737d67
fridego <931035186@qq.com>
2018-07-23 14:50:13 +0800

    refactor: median of two sorted arrays

--- leetcode/4-median-of-two-sorted-arrays.js
+++ leetcode/4-median-of-two-sorted-arrays.js
@@ -1,31 +1,32 @@
+/**
+ * 
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
 
+ * 请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
 
+ * 示例 1:
+
+ * nums1 = [1, 3]
+ * nums2 = [2]
+
+ * 中位数是 2.0
+ * 示例 2:
+
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+
+ * 中位数是 (2 + 3)/2 = 2.5
+ * 
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+ // 时间复杂度有待优化
 module.exports = function findMedianSortedArrays (nums1, nums2) {
-    let [l1, l2] = [nums1.length, nums2.length];
-    if(l1 > l2) [num1, nums2, l1, l2] = [nums2, nums1, l2, l1];
-    let total = l1 + l2;
-    let lstart = 0;
-    let end = l1 - 1;
-    let pos = ((lstart + end + 1) / 2 - 1) ^ 0;
-    let npos = ((total + 1) / 2) - pos - 2;
-    while(pos >= 0 && pos <= end) {
-        let [left, right] = [Math.max(nums1[pos], nums2[npos]), Math.max(nums1[pos + 1], nums2[npos + 1])];
-        if (left <= right) {
-            if(total % 2) {
-                return left;
-            } else {
-                return (left + rght) / 2;
-            }
-        } else {
-            if(nums1[pos] > nums2[npos]) {
-                end = pos - 1;    
-                pos = ((lstart + end + 1) / 2 - 1) ^ 0;
-                npos = ((total + 1) / 2) - pos - 2;
-            } else {
-                lstart = pos + 1;
-                pos = ((lstart + end + 1) / 2 - 1) ^ 0;
-                npos = ((total + 1) / 2) - pos - 2;
-            }
-        }
-    }
+	let total = nums1.length + nums2.length;
+    let arr = nums1.concat(nums2).sort((v1, v2) => (v1 - v2));
+    return total % 2 ? arr[(total - 1) / 2] : (arr[total / 2] + arr[total / 2 - 1]) / 2;
 }
